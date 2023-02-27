6f40f19a15f4da9e3fb845a7729bb2c471cd2751
Kevin Bowen <kevin.bowen@gmail.com>
2023-02-26 19:11:09 -0800

    Create initial sandbox templates for html, js, & css

---
+++ foundations/sandbox/static/js/fundamentals_pt1.js
@@ -0,0 +1,18 @@
+let name = "Kevin"; // define the variable
+
+admin = name;
+
+console.log(admin);
+// alert("Hello " + admin);
+let x = 100 + 50;
+console.log(x);
+
+console.log(23 + 97);
+console.log(1 + 2 + 3 + 4 + 5 + 6);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log("a equals " + a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
