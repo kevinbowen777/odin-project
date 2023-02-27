8405a772508efced83e23560eb419490536f41b2
Kevin Bowen <kevin.bowen@gmail.com>
2023-02-26 19:46:09 -0800

    Add js fundamentals exercises

---
+++ foundations/sandbox/static/js/fundamentals_pt2.js
@@ -0,0 +1,10 @@
+//let name = "Ilya";
+
+// alert( `hello ${1}` );
+// alert( `hello ${"name"}` );
+// alert( `hello ${name}` );
+
+const one = "Hello, ";
+const two = "how are you?";
+const joined = `${one}${two}`;
+console.log(joined); // "Hello, how are you?"
