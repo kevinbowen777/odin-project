daf7a163d7d5dbbee0389abccffe0c71dbdb6f3f
Kevin Bowen <kevin.bowen@gmail.com>
2023-02-26 19:46:59 -0800

    Add additional exercise pages & scripts

---
+++ foundations/sandbox/static/js/form_validate.js
@@ -0,0 +1,26 @@
+// Used by form_validate.html
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    username: /^[a-z\d]{5,12}$/i,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+    password: /^[\w@-]{8,20}$/i,
+    telephone: /^\d{10}$/,
+    slug: /^[a-z\d-]{8,20}$/
+};
+
+// validation function
+function validate(field, regex) {
+    if (regex.test(field.value)) {
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        //console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value])
+    });
+});
